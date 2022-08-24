4dea81d62dda2f8f4600a77c3a4c5f930319dbcd
Lokesh Vyavhare <107460761+LV-23@users.noreply.github.com>
2022-08-24 21:05:46 +0530

    Deleting Files

--- fw20_0254/Scripts/exercise-list.js
+++
@@ -1,7 +0,0 @@
-import grid_append from '../grid_category/cate_exer_grid.js';
-import { append_all_list } from '../exercise_list/exercises_appending.js';
-
-grid_append('exer_9cate_grid');
-
-append_all_list('exer_list');
-
