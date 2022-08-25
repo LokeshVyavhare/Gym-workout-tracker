1b80bc37b7248e16f308be5c1186dedd6c7592a9
LV-23 <lokeshvyavhare23@gmail.com>
2022-08-25 19:49:07 +0530

    Adding work day3

--- fw20_0254/Scripts/exercise-list.js
+++ fw20_0254/Scripts/exercise-list.js
@@ -1,10 +1,15 @@
 import grid_append from '../grid_category/cate_exer_grid.js';
-import { append_all_list } from '../exercise_list/exercises_appending.js';
+import { append_all_list, append_cate_list } from '../exercise_list/exercises_appending.js';
 import navbar2 from '../../Umer_Ahmad_fw20_0026/JS/nav.js';
-document.querySelector('header').innerHTML = navbar2();
+import exer_tabs from '../comm_exer_tabs/export.js';
 import { navbar, footer } from '../../components/navbar&footer.js'
 
+document.querySelector('header').innerHTML = navbar2();
+document.getElementById('comm_exr_tabs').innerHTML = exer_tabs();
+
 grid_append('exer_9cate_grid');
+
+
 
 document.querySelector('footer').innerHTML = footer();
 
--- fw20_0254/Scripts/exercise_category.js
+++ fw20_0254/Scripts/exercise_category.js
@@ -1,2 +1,20 @@
-let cate_exer = JSON.parse(localStorage.getItem('cate_exer'));
-console.log(cate_exer)
+import grid_append from '../grid_category/cate_exer_grid.js';
+import { append_all_list, append_cate_list } from '../exercise_list/exercises_appending.js';
+import navbar2 from '../../Umer_Ahmad_fw20_0026/JS/nav.js';
+import exer_tabs from '../comm_exer_tabs/export.js';
+import { navbar, footer } from '../../components/navbar&footer.js'
+
+document.querySelector('header').innerHTML = navbar2();
+document.getElementById('comm_exr_tabs').innerHTML = exer_tabs();
+grid_append('exer_9cate_grid');
+document.querySelector('footer').innerHTML = footer();
+
+let exer_cate = JSON.parse(localStorage.getItem('cate_exer'));
+
+
+append_cate_list('exer_list', exer_cate);;
+
+
+document.getElementById('all_exer_page').addEventListener('click', (event) => {
+    window.location.href = './exercise-list.html';
+})
