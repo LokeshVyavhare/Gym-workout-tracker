fbb6eed6e94faac79e4a129473afb4d65e502738
harshal-kitukale <harshalkitukale005@gmail.com>
2022-08-27 21:15:21 +0530

    add kr liya bhai

--- fw20_0729/Scripts/profilePage2.js
+++ fw20_0729/Scripts/profilePage2.js
@@ -17,51 +17,73 @@ function BMfunc() {
 let general=document.querySelector("#general").addEventListener("click",function(){
     generalfunc()})
 
-    function generalfunc() {
-        let general=document.getElementById("general-container")
-        general.classList.remove("not-display")
-        general.classList.add("display")
+function generalfunc() {
+    let general=document.getElementById("general-container")
+    general.classList.remove("not-display")
+    general.classList.add("display")
     
-        let BM=document.getElementById("Body_measurements-container")
-        console.log(BM);
-        BM.classList.remove("display")
-        BM.classList.add("not-display")
+    let BM=document.getElementById("Body_measurements-container")
+    console.log(BM);
+    BM.classList.remove("display")
+    BM.classList.add("not-display")
     
     }
 // BMfunc()
 
-
+// email data
 let userData=JSON.parse(localStorage.getItem("signup_email"))||[]
 document.getElementById("email-id").innerText=userData
 
 let users=JSON.parse(localStorage.getItem("users"))||[]
 
-
+// adding event listener
 let form=document.querySelector("form")
-.addEventListener("submit",(event)=>{
-// event.preventDefault()
+.addEventListener("submit",(event) => {
+        event.preventDefault()
+        addData(users)
+    })
+// above function
+function addData(users){
+    let name=document.getElementById("name1").value
+    let password=document.getElementById("password1").value
+    let conf_password=document.getElementById("password2").value
+users.forEach((ele,i) => {
+    
+    let obj={
+        "name":name,
+        "email":userData,
+        "password":password,
+        "conf_password":conf_password
+    
+    }
+    console.log(obj);
+    
+    if(userData==ele.email && name!==""){
+       users[i]["name"]=name
+       console.log(name)
+        localStorage.setItem("users",JSON.stringify(users))
 
+    }
+    else if(userData==ele.email && password!==""&& conf_password!==""){
 
-let name=document.getElementById("name1").value
-let password=document.getElementById("password1").value
-let conf_password=document.getElementById("password2").value
-
-let obj={
-    "name":name,
-    "email":userData,
-    "password":password,
-    "conf_password":conf_password
+        if(password===conf_password){
+        users[i]["password"]=password;
+        users[i]["conf_password"]=conf_password;
+        localStorage.setItem("users",JSON.stringify(users))
+        }
+        else
+        {
+        alert("password is incorrect")
+        }
+    }
+    
+    else{
+            users.push(obj)
+            localStorage.setItem("users",JSON.stringify(users))
+           
+    }
+});
 
 }
 
-if(password===conf_password){
-    users.push(obj)
-    localStorage.setItem("users",JSON.stringify(users))
-}
-else{
-    alert("password is incorrect")
-}
 
-
-
-})
