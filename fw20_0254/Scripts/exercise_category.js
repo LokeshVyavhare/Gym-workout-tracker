d9b81ac72ea2b4a8c7bf8475d911ecebb0cd9d64
LV-23 <lokeshvyavhare23@gmail.com>
2022-08-27 10:54:41 +0530

    added pop up function

--- fw20_0254/Scripts/exercise_category.js
+++ fw20_0254/Scripts/exercise_category.js
@@ -3,6 +3,7 @@ import { append_all_list, append_cate_list } from '../exercise_list/exercises_ap
 import navbar2 from '../../Umer_Ahmad_fw20_0026/JS/nav.js';
 import exer_tabs from '../comm_exer_tabs/export.js';
 import { navbar, footer } from '../../fw20_1110/components/navbar&footer.js'
+import { popA, popQ } from '../popup/pop.js'
 
 document.querySelector('header').innerHTML = navbar2();
 document.getElementById('comm_exr_tabs').innerHTML = exer_tabs();
@@ -15,6 +16,6 @@ let exer_cate = JSON.parse(localStorage.getItem('cate_exer'));
 append_cate_list('exer_list', exer_cate);;
 
 
-document.getElementById('all_exer_page').addEventListener('click', (event) => {
-    window.location.href = './exercise-list.html';
+document.getElementById('all_exer_page').addEventListener('click', async (event) => {
+    popQ('Successful login', 'True', 'False');
 })
---
+++ fw20_0254/popup/pop.js
@@ -0,0 +1,75 @@
+let blk_scr = () => {
+    let blk_scrn = document.createElement('div');
+    blk_scrn.id = 'blk_scrn';
+    document.querySelector('body').append(blk_scrn);
+}
+let hide_blk_scr = () => {
+    document.getElementById('blk_scrn').remove();
+}
+let popA = async (text, loc) => {
+
+
+    let popup = document.createElement('section');
+    popup.id = 'pop_up_alert_msg';
+    popup.innerText = text;
+    document.querySelector('body').append(popup);
+
+    document.getElementById('pop_up_alert_msg').classList.add('display');
+    document.getElementById('pop_up_alert_msg').classList.remove('notdisplay');
+    blk_scr();
+    setTimeout(() => {
+        hide('pop_up_alert_msg', 3000);
+        hide_blk_scr();
+        if (loc !== undefined) {
+            window.location.href = loc;
+        }
+    }, 3000);
+
+}
+let popQ = async (text, T, F) => {
+
+
+    let popup = document.createElement('section');
+    popup.id = 'pop_up_ask_msg'
+    popup.innerHTML = `
+        <p></p>
+        <div>
+        <div id='t_value'></div>
+        <div id='f_value'></div>
+        </div>
+        `
+    document.querySelector('body').append(popup);
+    document.getElementById('t_value').innerText = T;
+    document.getElementById('f_value').innerText = F;
+    document.querySelector('#pop_up_ask_msg p').innerText = text;
+    document.getElementById('t_value').addEventListener('click', () => {
+        localStorage.setItem('pop_up_ask_ans', JSON.stringify(true));
+        hide('pop_up_ask_msg', 500);
+    });
+    document.getElementById('f_value').addEventListener('click', () => {
+        localStorage.setItem('pop_up_ask_ans', JSON.stringify(false));
+        hide('pop_up_ask_msg', 500);
+
+    });
+
+    document.getElementById('pop_up_ask_msg').classList.add('display');
+    document.getElementById('pop_up_ask_msg').classList.remove('notdisplay');
+    blk_scr();
+
+
+}
+let hide = async (id, t) => {
+    setTimeout(() => {
+        document.getElementById(id).remove();
+        hide_blk_scr();
+    }, t);
+
+}
+
+export { popA, popQ };
+
+
+
+
+
+
