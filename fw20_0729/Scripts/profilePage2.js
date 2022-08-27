7d41c1736eb6dcd0c44b1c8f6657880c1fb529cd
LV-23 <lokeshvyavhare23@gmail.com>
2022-08-28 00:36:13 +0530

    Added update

--- fw20_0729/Scripts/profilePage2.js
+++ fw20_0729/Scripts/profilePage2.js
@@ -1,88 +1,103 @@
 
 
-let BM=document.querySelector("#Body_measurements").addEventListener("click",function(){
-    BMfunc()})
+let BM = document.querySelector("#Body_measurements").addEventListener("click", function () {
+    BMfunc()
+})
 
 function BMfunc() {
-    let general=document.getElementById("general-container")
+    let general = document.getElementById("general-container")
     general.classList.remove("display")
     general.classList.add("not-display")
 
-    let BM=document.getElementById("Body_measurements-container")
+    let BM = document.getElementById("Body_measurements-container")
     console.log(BM);
     BM.classList.remove("not-display")
     BM.classList.add("display")
 
 }
-let general=document.querySelector("#general").addEventListener("click",function(){
-    generalfunc()})
+let general = document.querySelector("#general").addEventListener("click", function () {
+    generalfunc()
+})
 
 function generalfunc() {
-    let general=document.getElementById("general-container")
+    let general = document.getElementById("general-container")
     general.classList.remove("not-display")
     general.classList.add("display")
-    
-    let BM=document.getElementById("Body_measurements-container")
+
+    let BM = document.getElementById("Body_measurements-container")
     console.log(BM);
     BM.classList.remove("display")
     BM.classList.add("not-display")
-    
-    }
+
+}
 // BMfunc()
 
 // email data
-let userData=JSON.parse(localStorage.getItem("signup_email"))||[]
-document.getElementById("email-id").innerText=userData
+let userData = JSON.parse(localStorage.getItem("signup_email")) || []
+document.getElementById("email-id").innerText = userData
 
-let users=JSON.parse(localStorage.getItem("users"))||[]
+let users = JSON.parse(localStorage.getItem("users")) || []
 
 // adding event listener
-let form=document.querySelector("form")
-.addEventListener("submit",(event) => {
-        event.preventDefault()
+let form = document.querySelector("form")
+    .addEventListener("submit", (event) => {
+        // event.preventDefault()
         addData(users)
     })
 // above function
-function addData(users){
-    let name=document.getElementById("name1").value
-    let password=document.getElementById("password1").value
-    let conf_password=document.getElementById("password2").value
-users.forEach((ele,i) => {
-    
-    let obj={
-        "name":name,
-        "email":userData,
-        "password":password,
-        "conf_password":conf_password
-    
-    }
-    console.log(obj);
-    
-    if(userData==ele.email && name!==""){
-       users[i]["name"]=name
-       console.log(name)
-        localStorage.setItem("users",JSON.stringify(users))
+function addData(users) {
+    let name = document.getElementById("name1").value
+    let password = document.getElementById("password1").value
+    let conf_password = document.getElementById("password2").value
 
-    }
-    else if(userData==ele.email && password!==""&& conf_password!==""){
+    if (users.length == 0) {
+        let obj = {
+            "name": name,
+            "email": userData,
+            "password": password,
+            "conf_password": conf_password
 
-        if(password===conf_password){
-        users[i]["password"]=password;
-        users[i]["conf_password"]=conf_password;
-        localStorage.setItem("users",JSON.stringify(users))
         }
-        else
-        {
-        alert("password is incorrect")
-        }
+
+        users.push(obj)
+        localStorage.setItem("users", JSON.stringify(users))
+    } else {
+        users.forEach((ele, i) => {
+
+            let obj = {
+                "name": name,
+                "email": userData,
+                "password": password,
+                "conf_password": conf_password
+
+            }
+            console.log(obj);
+
+            if (userData == ele.email && name !== "") {
+                users[i]["name"] = name
+                console.log(name)
+                localStorage.setItem("users", JSON.stringify(users))
+
+            }
+            else if (userData == ele.email && password !== "" && conf_password !== "") {
+
+                if (password === conf_password) {
+                    users[i]["password"] = password;
+                    users[i]["conf_password"] = conf_password;
+                    localStorage.setItem("users", JSON.stringify(users))
+                }
+                else {
+                    alert("password is incorrect")
+                }
+            }
+
+            else {
+                users.push(obj)
+                localStorage.setItem("users", JSON.stringify(users))
+
+            }
+        });
     }
-    
-    else{
-            users.push(obj)
-            localStorage.setItem("users",JSON.stringify(users))
-           
-    }
-});
 
 }
 
